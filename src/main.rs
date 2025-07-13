df7a8a3831b8ae27419c82414a36025593e33168
Robert M Hendricks <rmasonh25@gmail.com>
2025-07-13 14:42:46 -0400

    initial guess game

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// guessing game 
+use std::io;
+
+
+fn main() {
+    println!("Guess the Number");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed: {guess}");
+}
